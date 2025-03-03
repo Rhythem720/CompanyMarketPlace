7a99c7375664400d83d77949e064e3a173fa5c57
rhythem <rhythemmalviya789@gmail.com>
2025-03-03 10:51:50 +0530

    header componet

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,10 +1,16 @@
 import { Component } from '@angular/core';
-
+import { AuthComponent } from '../auth/auth.component';
+import { Router } from '@angular/router';
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.css']
 })
 export class HeaderComponent {
+  constructor( private router: Router) { }
 
+  logOut() {
+    localStorage.removeItem('currentuser'); 
+    this.router.navigateByUrl('/login');
+ }
 }
