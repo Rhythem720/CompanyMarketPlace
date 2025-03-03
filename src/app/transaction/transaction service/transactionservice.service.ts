50496d483df226c48ba77d4813eaa0b02d4f7d55
rhythem <rhythemmalviya789@gmail.com>
2025-03-03 16:49:20 +0530

    transaction service

---
+++ src/app/transaction/transaction service/transactionservice.service.ts
@@ -0,0 +1,44 @@
+import { Injectable } from '@angular/core';  
+
+interface Transaction {  
+  transactionid: number;  
+  amount: number;  
+  datetime: Date;  
+  type: string; // e.g., "buy"  
+  username: string;  
+  itemName: string;  
+  itemDescription: string;  
+}  
+
+@Injectable({  
+  providedIn: 'root'  
+})  
+export class TransactionService {  
+  private transactions: Transaction[] = [];  
+  private transactionIdCounter = 1;  
+  
+  constructor() {}  
+
+  addTransaction(transaction: Transaction) {  
+    debugger
+    
+    transaction.transactionid = this.transactionIdCounter++;  
+    const currentuser = localStorage.getItem("currentuser")
+    transaction.username=   currentuser?currentuser:"" ;
+    this.transactions.push(transaction); 
+    localStorage.setItem("transactions",JSON.stringify(this.transactions));
+  }  
+
+  getTransactions() {  
+    const localdata = localStorage.getItem("transactions");
+    const currentuser = localStorage.getItem("currentuser");
+    if(localdata!=null)
+    {
+       this.transactions = JSON.parse(localdata);
+       return this.transactions.filter(x=>x.username==currentuser);
+    }
+    else
+     return this.transactions;
+    
+  }  
+}  
